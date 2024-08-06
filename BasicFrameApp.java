57927606e414d9bfac0bc69116f470f08810b828
Mathebula-Peace <peacekurhula038@gmail.com>
2024-08-06 17:38:50 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,13 @@
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+public class BasicFrameApp {
+
+    public static void main(String[] args) {
+        MyFirstFrame myFrame;
+        
+        myFrame = new MyFirstFrame();
+    }
+    
+}
